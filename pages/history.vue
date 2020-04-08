32e1f6885ba98e824dce5d68bbd2bee764e3251a
Adam English <englishadam@hotmail.co.uk>
2020-04-08 18:19:31 +0100

    Added pages

---
+++ pages/history.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        history
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Search History',
+      meta: [{ hid: 'description', name: 'description', content: 'Weather search histroy.' }]
+    }
+  }
+}
+</script>
---
+++ pages/search.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fill-height fluid class="pa-0">
+    <v-row no-gutters class="fill-height" align="start" justify="center">
+      <v-col
+        :class="{ shrink: $vuetify.breakpoint.smAndDown }"
+        :align-self="$vuetify.breakpoint.smAndDown"
+        cols="12"
+        sm="12"
+        md="6"
+      >
+        <search-form />
+        {{ $vuetify.breakpoint.smAndDown }}
+      </v-col>
+      <!-- 
+        Left Column
+       -->
+      <v-col :align-self="$vuetify.breakpoint.smAndDown" sm="12" md="6">
+        <pixijs />
+      </v-col>
+      <!-- 
+        Right Column
+       -->
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import SearchForm from '~/components/SearchForm.vue'
+import Pixijs from '~/components/Pixijs.vue'
+
+export default {
+  components: {
+    SearchForm,
+    Pixijs
+  },
+
+  head() {
+    return {
+      title: 'Weather Search',
+      meta: [{ hid: 'description', name: 'description', content: 'Search for weather by city' }]
+    }
+  }
+}
+</script>
