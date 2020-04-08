f626e2481c66cdaf6af445a6a0e76469e2c0af35
Adam English <englishadam@hotmail.co.uk>
2020-04-08 18:32:12 +0100

    Fixed responsive layout

--- pages/search.vue
+++ pages/search.vue
@@ -1,27 +1,41 @@
 <template>
-  <v-container fill-height fluid class="pa-0">
+  <v-container v-if="!$vuetify.breakpoint.smAndDown" fill-height fluid class="pa-0 align-start">
     <v-row no-gutters class="fill-height" align="start" justify="center">
-      <v-col
-        :class="{ shrink: $vuetify.breakpoint.smAndDown }"
-        :align-self="$vuetify.breakpoint.smAndDown"
-        cols="12"
-        sm="12"
-        md="6"
-      >
+      <v-col cols="6">
         <search-form />
         {{ $vuetify.breakpoint.smAndDown }}
       </v-col>
-      <!-- 
-        Left Column
-       -->
-      <v-col :align-self="$vuetify.breakpoint.smAndDown" sm="12" md="6">
+      <!-- Left Column -->
+
+      <v-col class="fill-height" cols="6">
         <pixijs />
       </v-col>
-      <!-- 
-        Right Column
-       -->
+      <!-- Right Column -->
     </v-row>
   </v-container>
+  <!-- 
+    Medium and large devices
+  -->
+
+  <v-container v-else fill-height fluid class="pa-0 align-start">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <search-form />
+        {{ $vuetify.breakpoint.smAndDown }}
+      </v-col>
+    </v-row>
+    <!-- Top Row -->
+
+    <v-row no-gutters class="fill-height">
+      <v-col cols="12">
+        <pixijs />
+      </v-col>
+    </v-row>
+    <!-- Top Row -->
+  </v-container>
+  <!-- 
+    Small devices and below
+  -->
 </template>
 
 <script>
