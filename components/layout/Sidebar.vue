6b712e8e3d2c08983e6cdf696d90bcdf924cfa05
Adam English <englishadam@hotmail.co.uk>
2020-04-08 17:39:59 +0100

    Removed unecessary

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col v-for="(item, i) in items" :key="i" class="shrink">
+      <v-btn :to="item.to" text>
+        {{ item.title }}
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // Navigation items
+      items: [
+        {
+          title: 'Search',
+          icon: 'search',
+          to: 'search'
+        },
+        {
+          title: 'History',
+          icon: 'history',
+          to: 'history'
+        }
+      ]
+    }
+  }
+}
+</script>
