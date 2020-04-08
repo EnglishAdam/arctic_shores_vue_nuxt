79b3bfec5e5b164076fc8dec6f24133205ae5de2
Adam English <englishadam@hotmail.co.uk>
2020-04-09 00:24:37 +0100

    Updated pages

--- pages/history.vue
+++ pages/history.vue
@@ -1,15 +1,63 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        history
+  <v-container v-if="!$vuetify.breakpoint.smAndDown" fill-height fluid class="pa-0 align-start">
+    <v-row no-gutters class="fill-height" align="start" justify="center">
+      <v-col cols="6">
+        <history />
       </v-col>
+      <!-- Left Column -->
+
+      <v-col class="fill-height grey lighten-3" cols="6">
+        <pixijs :search="getLastSelected" />
+      </v-col>
+      <!-- Right Column -->
     </v-row>
   </v-container>
+  <!-- 
+    Medium and large devices
+  -->
+
+  <v-container v-else fill-height fluid class="pa-0 align-start">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <history />
+        {{ $vuetify.breakpoint.smAndDown }}
+      </v-col>
+    </v-row>
+    <!-- Top Row -->
+
+    <v-divider></v-divider>
+
+    <v-row no-gutters class="fill-height grey lighten-3">
+      <v-col cols="12">
+        <pixijs :search="getLastSelected" />
+      </v-col>
+    </v-row>
+    <!-- Top Row -->
+  </v-container>
+  <!-- 
+    Small devices and below
+  -->
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import History from '~/components/History.vue'
+import Pixijs from '~/components/Pixijs.vue'
+
 export default {
+  layout: 'main',
+
+  components: {
+    History,
+    Pixijs
+  },
+
+  computed: {
+    ...mapGetters({
+      getLastSelected: 'weather/getLastSelected'
+    })
+  },
+
   head() {
     return {
       title: 'Search History',
--- pages/search.vue
+++ pages/search.vue
@@ -3,12 +3,11 @@
     <v-row no-gutters class="fill-height" align="start" justify="center">
       <v-col cols="6">
         <search-form />
-        {{ $vuetify.breakpoint.smAndDown }}
       </v-col>
       <!-- Left Column -->
 
-      <v-col class="fill-height" cols="6">
-        <pixijs />
+      <v-col class="fill-height grey lighten-3" cols="6">
+        <pixijs :search="getLastSearch" />
       </v-col>
       <!-- Right Column -->
     </v-row>
@@ -26,9 +25,11 @@
     </v-row>
     <!-- Top Row -->
 
-    <v-row no-gutters class="fill-height">
+    <v-divider></v-divider>
+
+    <v-row no-gutters class="fill-height grey lighten-3">
       <v-col cols="12">
-        <pixijs />
+        <pixijs :search="getLastSearch" />
       </v-col>
     </v-row>
     <!-- Top Row -->
@@ -39,13 +40,22 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import SearchForm from '~/components/SearchForm.vue'
 import Pixijs from '~/components/Pixijs.vue'
 
 export default {
+  layout: 'main',
+
   components: {
     SearchForm,
     Pixijs
+  },
+
+  computed: {
+    ...mapGetters({
+      getLastSearch: 'weather/getLastSearch'
+    })
   },
 
   head() {
